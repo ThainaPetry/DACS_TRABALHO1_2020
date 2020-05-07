5fb7f4fe32d88dbaeb55d799840555ce24b106fc
POO - Banco <49696402+ThainaPetry@users.noreply.github.com>
2020-05-07 20:42:14 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'univilleDataSet.clientes' table. You can move, or remove it, as needed.
+            this.clientesTableAdapter.Fill(this.univilleDataSet.clientes);
+
+        }
+
+        private void bindingNavigator1_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            this.clientesTableAdapter.Update(this.univilleDataSet.clientes);
+        }
+    }
+}
